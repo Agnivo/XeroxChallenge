b4cbea5df9cf1b94d5ed4e8bdf69b5b244934858
Biswajit Paria <biswajitsc@gmail.com>
2015-10-17 11:18:47 +0530

    rnn experimenting

--- run_model_BP.py
+++ run_model_BP.py
@@ -1,13 +1,10 @@
+import numpy as nt
 import pandas as pa
-import numpy as np
-from library import features as F
 
-
+def readfeats():
+    
 def main():
-    F.getfeatures()
-
-    # print tfeats.shape, ttargets.shape, vfeats.shape, vtargets.shape
-    # print feats, targets
+    
 
 if __name__ == '__main__':
     main()
